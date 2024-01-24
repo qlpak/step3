dc3520ed7357bf0806d8aac286d34a2c8bf4ff53
Łukasz Kulpaczyński <lukasz18kulpaczynski@gmail.com>
2024-01-24 17:00:05 +0100

    dodanie kodu w py

--- step3.py
+++ step3.py
@@ -1,1 +1,8 @@
 print("step3 test")
+
+liczba = int(input("Podaj liczbę: "))
+if liczba %2 == 0:
+    print("Podana liczba jest podzielna przez 2")
+else:
+    print("Podana liczba nie jest podzielna przez 2")
+
